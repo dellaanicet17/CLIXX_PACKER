fa3144f6f8bbc44ae5b70efede44c651ebf6ce70
dellaanicet17 <dellaanicet17@gmail.com>
2024-10-26 14:01:05 -0400

    Update inspector.tf

--- instances/inspector.tf
+++ instances/inspector.tf
@@ -5,25 +5,25 @@ resource "aws_inspector_resource_group" "stack_res" {
   }
   depends_on = [aws_instance.application_server]
 }
-
+ 
 resource "aws_inspector_assessment_target" "assessment" {
   name               = "Hardening Assessment"
   resource_group_arn = aws_inspector_resource_group.stack_res.arn
 }
-
+ 
 resource "aws_inspector_assessment_template" "stack_hardening_rules" {
   name       = "stack_rules"
   target_arn = aws_inspector_assessment_target.assessment.arn
   duration   = 3600
-
-  rulesPackageArns = [
-        "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-PmNV0Tcd",
-        "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-R01qwB5Q",
-        "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-gEjTy7T7",
-        "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-rExsr2X8"
-    ]
+ 
+  rules_package_arns = [
+    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-PmNV0Tcd",
+    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-R01qwB5Q",
+    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-gEjTy7T7",
+    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-rExsr2X8"
+  ]
 }
-
+ 
 output "assessment_template_arn" {
     value=aws_inspector_assessment_template.stack_hardening_rules.arn
 }
