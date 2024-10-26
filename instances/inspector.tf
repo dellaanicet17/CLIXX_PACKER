15cdd46f9d201b899e7df40de69cf23987e7f068
dellaanicet17 <dellaanicet17@gmail.com>
2024-10-26 12:56:52 -0400

    Update inspector.tf

--- instances/inspector.tf
+++ instances/inspector.tf
@@ -16,12 +16,12 @@ resource "aws_inspector_assessment_template" "stack_hardening_rules" {
   target_arn = aws_inspector_assessment_target.assessment.arn
   duration   = 3600
 
-  rules_package_arns = [
-    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-gEjTy7T7",
-    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-rExsr2X8",
-    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-PmNV0Tcd",
-    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-R01qwB5Q",
-  ]
+  "rulesPackageArns": [
+        "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-PmNV0Tcd",
+        "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-R01qwB5Q",
+        "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-gEjTy7T7",
+        "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-rExsr2X8"
+    ]
 }
 
 output "assessment_template_arn" {
